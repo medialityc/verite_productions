dc6a92ef6d4c3ce484f37d9b7305951180ea94d2
Kristian Carreño <kristiancarreno02@gmail.com>
2025-01-31 22:12:54 -0500

    as

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,11 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-    return {
-        rules: {
-            userAgent: '*',
-            allow: '/'
-        },
-        sitemap: 'https://veriteproducciones.medialityc.com/sitemap.xml'
-    }
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://producciones.ass.com/sitemap.xml",
+  };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,13 @@
-import type { MetadataRoute } from 'next'
-import React from 'react';
+import type { MetadataRoute } from "next";
+import React from "react";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    return [
-        {
-            url: 'https://veriteproducciones.medialityc.com',
-            lastModified: new Date(),
-            changeFrequency: 'daily',
-            priority: 1
-        }
-    ]
+  return [
+    {
+      url: "https://producciones.ass.com",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+  ];
 }
